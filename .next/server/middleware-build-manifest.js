5afd3f08843e862fce251bb42f91190b8eeded5f
Kane Traylor <kanetraylor556@gmail.com>
2025-05-05 14:10:07 -0400

    Maybe I forgot to commit stuff

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,10 +24,10 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/dashboard/account": [
+    "/onboarding/signup": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/dashboard/account.js"
+      "static/chunks/pages/onboarding/signup.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8c15df48cd1c51ef"; }
+/******/ 		__webpack_require__.h = function() { return "bb11c1357eb85016"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
